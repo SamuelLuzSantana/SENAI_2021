3daa57c50f4067e16e0eb02c894d5c05f2f8393c
Samuel Luz Santana <35834520898@info.local>
2021-04-21 14:13:08 -0300

    interfaces parcialmente finalizadas

---
+++ sprint2_BackEnd/inLock/API_INLOCK/Senai_inLock_tarde/Senai_inLock_tarde/Repositories/UsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_inLock_tarde.Repositories
+{
+    public class UsuarioRepository
+    {
+    }
+}
